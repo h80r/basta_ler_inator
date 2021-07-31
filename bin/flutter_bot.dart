0e71cf8d45445a046b94bc89b6b1ed89482e0d67
h80r <h80rgbc@gmail.com>
2021-07-31 15:00:48 -0300

    feature/bot_base | Add bot structure

--- bin/flutter_bot.dart
+++ bin/flutter_bot.dart
@@ -1,3 +1,22 @@
+import 'package:nyxx/nyxx.dart';
+import 'package:dotenv/dotenv.dart' as dotenv;
+
 void main(List<String> arguments) {
-  print('Hello world!');
+  dotenv.load();
+
+  final bot = Nyxx(
+    dotenv.env['DISCORD_TOKEN']!,
+    GatewayIntents.allUnprivileged,
+  );
+
+  bot.onReady.listen((event) {
+    print('${bot.self.username} has connected to Discord!');
+  });
+
+  bot.onMessageReceived.listen((event) {
+    if (event.message.author.id == bot.self.id) return;
+    if (event.message.content.startsWith('basta ler')) {
+      event.message.channel.sendMessage(MessageBuilder.content('basta ler'));
+    }
+  });
 }
