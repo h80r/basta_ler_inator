e314c6ab4c449d585e2d99f9363c06e00c7a1497
h80r <h80rgbc@gmail.com>
2021-07-31 18:24:40 -0300

    feature/create_embed | Add extension docs
    - Adicionei um comentário de documentação pra função criada.

--- bin/utils/date_time_ex.dart
+++ bin/utils/date_time_ex.dart
@@ -1,4 +1,7 @@
 extension TimeReading on DateTime {
+  /// Returns the time as a string.
+  ///
+  /// Returns a string with the format "HH:MM:SS"
   String readableTime() {
     return '$hour:$minute:$second';
   }
