d795de78d27550e1a34e158349c3ef3ca8c08838
h80r <h80rgbc@gmail.com>
2021-07-31 17:18:40 -0300

    feature/flutter_search | Add Result model
    - Adicionei o modelo que será retornado das buscas.

---
+++ bin/models/result.dart
@@ -0,0 +1,11 @@
+class Result {
+  final String title;
+  final String description;
+  final String url;
+
+  Result({
+    required this.title,
+    required this.description,
+    required this.url,
+  });
+}
