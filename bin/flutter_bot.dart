a91581cf94d748467be054b1653405185130e303
h80r <h80rgbc@gmail.com>
2021-07-31 18:25:24 -0300

    feature/create_embed | Add create/delete embed
    - Adicionei comandos pra busca e deleção dos resultados

--- bin/flutter_bot.dart
+++ bin/flutter_bot.dart
@@ -1,7 +1,8 @@
 import 'package:nyxx/nyxx.dart';
 import 'package:dotenv/dotenv.dart' as dotenv;
 
-import 'flutter_search.dart';
+import 'utils/flutter_search.dart';
+import 'utils/date_time_ex.dart';
 
 void main(List<String> arguments) {
   dotenv.load();
@@ -15,20 +16,38 @@ void main(List<String> arguments) {
     print('${bot.self.username} has connected to Discord!');
   });
 
-  bot.onMessageReceived.listen((event) {
-    if (event.message.author.id == bot.self.id) return;
-    if (event.message.content.startsWith('basta ler')) {
-      search(event.message.content).then(
-        (value) => value?.forEach(
-          (element) => event.message.channel.sendMessage(
-            MessageBuilder.embed(
-              EmbedBuilder()
-                ..title = element.title
-                ..description = element.description
-                ..url = element.url,
+  bot.onMessageReceived.listen((event) async {
+    final msg = event.message;
+    final channel = msg.channel;
+
+    if (msg.author.id == bot.self.id) return;
+
+    if (msg.content == 'tomou um basta ler') {
+      await channel
+          .sendMessage(MessageBuilder.content(msg.createdAt.readableTime()));
+      final toDelete = await channel
+          .downloadMessages(limit: 20, before: msg.id)
+          .where((message) => message.author.id == bot.self.id)
+          .toList();
+      toDelete.forEach((message) => message.delete());
+    }
+
+    if (msg.content.startsWith('basta ler')) {
+      await search(msg.content).then(
+        (value) {
+          channel.startTypingLoop();
+          value?.forEach(
+            (element) => channel.sendMessage(
+              MessageBuilder.embed(
+                EmbedBuilder()
+                  ..title = element.title
+                  ..description = element.description
+                  ..url = element.url,
+              ),
             ),
-          ),
-        ),
+          );
+          channel.stopTypingLoop();
+        },
       );
     }
   });
